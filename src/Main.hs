28e53d9933fa42d7a4e37098527236ace75500fe
Jens Petersen <juhpetersen@gmail.com>
2026-03-27 19:24:09 +0800

    add Parser TextReq to separate new language --sample-text option

--- src/Main.hs
+++ src/Main.hs
@@ -3,51 +3,70 @@
 import Control.Monad.Extra (filterM, forM_, unless, when, whenJust)
 import qualified Data.ByteString as B
 import Data.Char (ord)
-import qualified Data.List as L
+import Data.List (singleton)
 import Data.Maybe (fromMaybe, isNothing)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified GI.Pango as Pango
 import qualified GI.PangoCairo.Interfaces.FontMap as PangoCairo
 import SimpleCmd (error', (+-+))
-import SimpleCmdArgs hiding (str)
-import System.Environment (getArgs, withArgs)
+import SimpleCmdArgs (flagWith', optional, simpleCmdArgs, some, strArg,
+                      strOptionWith, switchWith, (<|>))
 import Text.Printf (printf)
 import qualified Unicode.Char.General.Names as UN
 import qualified Unicode.Char.General.Scripts as US
 
 import Paths_fontwhich (version)
 
-main :: IO ()
-main = do
-  args <- getArgs
-  if null args
-    then withArgs ["--help"] main'
-    else main'
+data LangText = SampleText -- | LangName
 
-main' :: IO ()
-main' =
+data TextReq =
+  LangReq !String !(Maybe LangText) | InputText ![String]
+
+main :: IO ()
+main =
   simpleCmdArgs (Just version) "fontwhich"
   "Describes the fonts used to render text with pango" $
     run
     <$> optional (strOptionWith 'f' "font" "FONT" "Base font [default: Sans]")
-    <*> optional (strOptionWith 'l' "lang" "LANG" "Language code")
     <*> switchWith 'b' "utf8" "Output UTF-8 hex codes"
     <*> switchWith 'u' "unicode" "Output Unicode data"
-    <*> many (strArg "TEXT")
+    <*> textReqOpt
+  where
+    textReqOpt =
+      (LangReq
+       <$> strOptionWith 'l' "lang" "LANG" "Language code"
+       <*> optional (flagWith' SampleText 's' "sample-text" "Use Pango sample text for language"
+                     -- <|> flagWith' LangName 'n' "lang-name" "Use language name as text"
+                    )
+      )
+      <|>
+      InputText <$> some (strArg "TEXT")
 
-run :: Maybe String -> Maybe String -> Bool -> Bool -> [String] -> IO ()
-run mfont mlang hex unicode txt = do
+run :: Maybe String -> Bool -> Bool -> TextReq -> IO ()
+run mfont hex unicode txtreq = do
   -- Get a default Font Map and Context
   fontMap <- PangoCairo.fontMapGetDefault
   context <- Pango.fontMapCreateContext fontMap
   let baseName = fromMaybe "Sans" mfont
   baseFont <- Pango.fontDescriptionFromString $ T.pack baseName
-  mplang <- Pango.languageFromString $ T.pack <$> mlang
+  (txt,mlang) <-
+    case txtreq of
+      InputText args -> return (args,Nothing)
+      LangReq lang mltxt -> do
+        mplang <- Pango.languageFromString $ Just $ T.pack lang
+        t <-
+          case mltxt of
+            Nothing -> return []
+            Just SampleText ->
+              words . T.unpack <$> Pango.languageGetSampleString mplang
+            -- FIXME with iso-codes json + gettext perhaps
+            -- Just LangName -> return ["To be implemented!"]
+        return (t,mplang)
 
   if null txt then do
-      case mplang of
-        Nothing -> error' "no language determined or text given"
+      case mlang of
+        Nothing -> error' "no language or text string specified"
         Just plang -> do
           mfontset <- Pango.fontMapLoadFontset fontMap context baseFont plang
           case mfontset of
@@ -76,7 +95,7 @@ run mfont mlang hex unicode txt = do
       let myText = T.pack $ unwords txt
 
       Pango.contextSetFontDescription context $ Just baseFont
-      Pango.contextSetLanguage context mplang
+      Pango.contextSetLanguage context mlang
 
       let utf8Bytes = TE.encodeUtf8 myText
       when (hex || unicode) $
@@ -103,7 +122,7 @@ run mfont mlang hex unicode txt = do
 
 surround :: Char -> Char -> String -> String
 surround o c s =
-  o : s ++ L.singleton c
+  o : s ++ singleton c
 
 quoteStr :: String -> String
 quoteStr = surround '\'' '\''
